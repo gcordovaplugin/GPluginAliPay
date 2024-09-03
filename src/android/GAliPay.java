e9522ac6a2f2454784a636d457ad9ed04135ba99
吃掉大象的蚂蚁 <eoe2005@qq.com>
2024-09-03 11:12:40 +0800

    支付宝支付功能

--- src/android/GAliPay.java
+++ src/android/GAliPay.java
@@ -1,32 +1,82 @@
 package cn.yingzhichu.cordova.galipay;
 
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+
+import com.alipay.sdk.app.PayTask;
+
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Map;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class GAliPay extends CordovaPlugin {
 
+    private Handler handler;
+    private CallbackContext callback;
+
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(@NonNull Message msg) {
+                if(callback == null){
+                    return true;
+                }
+                PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                /**
+                 * 对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                 */
+                String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                String resultStatus = payResult.getResultStatus();
+                // 判断resultStatus 为9000则代表支付成功
+                if (TextUtils.equals(resultStatus, "9000")) {
+                    // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                    PluginResult rs = new PluginResult(PluginResult.Status.OK);
+                    rs.setKeepCallback(true);
+                    callback.sendPluginResult(rs);
+                } else {
+                    // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                    PluginResult rs = new PluginResult(PluginResult.Status.ERROR);
+                    rs.setKeepCallback(true);
+                    callback.sendPluginResult(rs);
+                }
+                return true;
+            }
+        });
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        callback = callbackContext;
+        String orderinfo = args.getString(0);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                PayTask alipay = new PayTask(cordova.getActivity());
+                Map<String, String> result = alipay.payV2(orderinfo, true);
+                Message msg = new Message();
+                msg.what = 0;
+                msg.obj = result;
+                handler.sendMessage(msg);
+            }
+        }).start();
+        return true;
     }
+
 }
